b93459c13d25a1e3885a3c0b21a8333cfc7c7973
genesismeli <genesismeli.96@gmail.com>
2023-03-11 01:41:53 -0300

    MS

--- gateway/src/main/java/com/dh/apigateway/ApiGatewayApplication.java
+++ gateway/src/main/java/com/dh/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.dh.elkin.apigateway;
+package com.dh.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
