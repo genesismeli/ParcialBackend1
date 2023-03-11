76b6527e94467f227fc7d00c092ee8bfeefe2706
genesismeli <genesismeli.96@gmail.com>
2023-03-11 01:56:08 -0300

    correccion gateway

--- gateway/src/test/java/com/dh/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/dh/gateway/GatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package com.dh.apigateway;
+package com.dh.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGatewayApplicationTests {
+class GatewayApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
