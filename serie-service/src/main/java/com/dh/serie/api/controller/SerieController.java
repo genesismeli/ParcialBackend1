dca8925f5eaf6dc8cffd27cf4123f954d0adc7ab
genesismeli <genesismeli.96@gmail.com>
2023-04-07 16:36:51 -0300

    Update configuration-parcial

---
+++ serie-service/src/main/java/com/dh/serie/api/controller/SerieController.java
@@ -0,0 +1,36 @@
+package com.dh.serie.api.controller;
+
+import com.dh.serie.model.Serie;
+import com.dh.serie.service.SerieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/series")
+public class SerieController {
+
+    private final SerieService serieService;
+
+    @GetMapping
+    public List<Serie> getAll() {
+        return serieService.getAll();
+    }
+
+    @GetMapping("/{genre}")
+    public List<Serie> getSerieByGenre(@PathVariable String genre) {
+        return serieService.getSeriesBygGenre(genre);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@RequestBody Serie serie) {
+        serieService.createSerie(serie);
+        return serie.getId();
+    }
+
+
+}
